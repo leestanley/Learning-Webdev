ba86115e024dfe2cd5b21e0598189434983f75d6
leestanley <stanleylee91776@gmail.com>
2019-07-23 10:48:24 -0700

    different jsx file for counters

--- JS/React/boilerplate/my-app/src/index.js
+++ JS/React/boilerplate/my-app/src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Counter from "./components/counter"
+import Counters from "./components/counters"
 import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<Counters />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
